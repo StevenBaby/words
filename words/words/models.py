f5984253c7a7c48ae9bc2ad9af5155777ae8dc9f
Steven Kang <kangweibaby@163.com>
2019-05-13 18:44:52 +0800

    fix user control

--- words/words/models.py
+++ words/words/models.py
@@ -298,20 +298,20 @@ class UserProfile(models.Model):
         verbose_name_plural = _('UserProfiles')
 
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         UserProfile.objects.create(user=instance)
 
 
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    UserProfile.objects.get_or_create(user=instance)
-    instance.profile.save()
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     UserProfile.objects.get_or_create(user=instance)
+#     instance.profile.save()
 
 
-@receiver(post_init, sender=User)
-def init_user_profile(sender, instance, **kwargs):
+# @receiver(post_init, sender=User)
+# def init_user_profile(sender, instance, **kwargs):
 
-    if not hasattr(instance, "profile") and instance.id:
-        instance.save()
+#     if not hasattr(instance, "profile") and instance.id:
+#         instance.save()
