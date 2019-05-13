2d785192c2b6cb98a3d993324dba4000376e6f03
Steven Kang <kangweibaby@163.com>
2019-05-13 18:54:32 +0800

    update database

--- words/settings/settings.py
+++ words/settings/settings.py
@@ -229,7 +229,7 @@ DATE = "%Y-%m-%d"
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "viewer", "static"),
