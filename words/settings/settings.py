a764d9f4e16374c4aec6a1692ccdc8018b249549
Steven Kang <kangweibaby@163.com>
2019-05-13 18:22:53 +0800

    add domain

--- words/settings/settings.py
+++ words/settings/settings.py
@@ -43,6 +43,7 @@ ALLOWED_HOSTS = [
     "localhost",
     "words.local",
     "steven-arch.local",
+    "words.ccyg.studio",
 ]
 
 
@@ -228,6 +229,7 @@ DATE = "%Y-%m-%d"
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "viewer", "static"),
